bf2798da215c951fa2c8b5375bc7d1afcac67999
gblh <746277418@qq.com>
2017-03-21 12:20:21 +0800

    joystick

--- tss1/src/app.js
+++ tss1/src/app.js
@@ -2,6 +2,7 @@ var self = null;
 var HelloWorldLayer = cc.Layer.extend({
     _mapLayer:null,
     _schedule: new CSchedule(),
+    _touchYaoB: false,
     ctor:function () {
         //////////////////////////////
         // 1. super init first
@@ -15,19 +16,17 @@ var HelloWorldLayer = cc.Layer.extend({
         this._mapLayer.addChild(snakeLayer, 1);
         SnakeManager.getInstance().setBodyParent(snakeLayer);
         this._playerId = SnakeManager.getInstance().createSnake(cc.p(200, 400), cc.p(1,0), 5, SnakeType.PLAYER);
-        gl.logDate("___2222__________________________")
         for( var i = 0; i < SNAKE_ENEMY_INIT_NUM; i++ ) {
             var posx = Math.random() * (MAP_WIDTH - 200);
             SnakeManager.getInstance().createSnake(cc.p(posx + 100, Math.random() * (MAP_HEIGHT - 200 ) + 100), cc.p(1,0), SNAKE_INIT_LENGTH, SnakeType.AI);
         }
-        gl.logDate("___qqqqq__________________________")
 
         var foodLayer = new cc.Layer();
         this._mapLayer.addChild(foodLayer, 0);
         FoodManager.getInstance().setBodyParent(foodLayer);
         FoodManager.getInstance().initFoods(300);
 
-        addNodeTouchEventListener(this, this.__nodeTouchEventListener);
+        addNodeTouchEventListener(this, this.__nodeTouchEventListener.bind(this));
         this._schedule.start(0, 0.01, this.__updateListener);
 
         //
@@ -43,25 +42,74 @@ var HelloWorldLayer = cc.Layer.extend({
         this.addChild(button, 2);
         button.setPosition(cc.p(cc.winSize.width - 50, 50));
 
+        //摇杆
+        this._diZuo = new cc.Sprite("res/joystick1.png");
+        this._diZuo.x = 100;
+        this._diZuo.y = 100;
+        this.addChild(this._diZuo);
+
+        this._hKuai = new cc.Sprite("res/joystick2.png");
+        this._hKuai.x = 100;
+        this._hKuai.y = 100;
+        this.addChild(this._hKuai);
+
+        this._hkInitPos = cc.p(100, 100);
+
+        this._radius = W(this._diZuo) / 2;
+
         return true;
     },
 
     __nodeTouchEventListener: function(touch, event)
     {
-        var SnakeFunc = function(){
+        var SnakeFunc = function(pos){
             var snake = SnakeManager.getInstance().getSnake(event.node._playerId);
             if (snake) {
-                var tWpos = event.node.convertToNodeSpace(event.location);
+                var tWpos = event.node.convertToNodeSpace(pos);
                 var sWpos = event.node.convertToNodeSpace(snake.getWoldPosition());
-                var dir = gl.pSub(tWpos, sWpos);
+                var dir = gl.pSub(tWpos, cc.p(100, 100));
                 snake.rotateTo(dir);
             }
         }
         if (event.name == "began"){
-            SnakeFunc();
+            if (event.x < 100+this._radius && event.x > 100-this._radius && event.y < 100+this._radius && event.y > 100-this._radius){
+                this._touchYaoB = true;
+            }
             return true;
         } else if (event.name == "moved"){
-            SnakeFunc();
+            if (this._touchYaoB){
+                var dis = gl.pGetDistance(this._hkInitPos, event.location);
+                var numX = event.x - this._hkInitPos.x;
+                var numY = event.y - this._hkInitPos.y;
+                if (dis < this._radius){
+                    this._hKuai.setPosition(event.location);
+                    SnakeFunc(event.location);
+                } else{
+                    var bili = this._radius / dis;
+                    //四大象限
+                    var x = 0;
+                    var y = 0;
+                    if (numX > 0 && numY > 0){
+                        x = (event.x - 100) * bili + 100;
+                        y = (event.y - 100) * bili + 100;
+                    } else if (numX < 0 && numY > 0){
+                        x = 100 - (100 - event.x) * bili;
+                        y = (event.y - 100) * bili + 100;
+                    } else if (numX < 0 && numY < 0){
+                        x = 100 - (100 - event.x) * bili;
+                        y = 100 - (100 - event.y) * bili;
+                    } else if (numX > 0 && numY < 0){
+                        y = 100 - (100 - event.y) * bili;
+                        x = (event.x - 100) * bili + 100;
+                    }
+                    
+                    this._hKuai.setPosition(cc.p(x, y));
+                    SnakeFunc(cc.p(x, y));
+                }
+            }
+        } else if (event.name == "ended"){
+            this._touchYaoB = false;
+            this._hKuai.setPosition(this._hkInitPos);
         }
     },
 
